8a6497d6788eee2b41bfd02ea1aaf39ac9ec33ec
CescDev <medicalsos068@gmail.com>
2025-07-15 13:02:21 -0600

    Agregando más url para evitar error de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,11 @@
 import { CorsOptions } from "cors";
 
-
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whiteList = [process.env.FRONTEND_URL];
+        const whiteList = [
+            process.env.FRONTEND_URL,
+            process.env.BACKEND_URL,
+        ];
 
         if(process.argv[2] === "--api") {
             whiteList.push(undefined)
