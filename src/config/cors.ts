d2e963855a5d97dc694aa01d4b304d7bb8b09599
CescDev <medicalsos068@gmail.com>
2025-07-16 11:42:42 -0600

    Cambiando cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,19 @@
-import cors from 'cors';
+import { CorsOptions } from "cors";
 
-const whiteList = [
-  process.env.FRONTEND_URL,
-];
 
-export const corsOptions: cors.CorsOptions = {
-  origin: function (origin, callback) {
+export const corsConfig: CorsOptions = {
+    origin: function (origin, callback) {
+        const whiteList = [process.env.FRONTEND_URL];
 
-    if (!origin || whiteList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+        if(process.argv[2] === "--api") {
+            whiteList.push(undefined)
+        }
+
+        if(whiteList.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Error de CORS"));
+        }
     }
-  },
-  credentials: true,
-};
+}
